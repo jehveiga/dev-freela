e53920c586059ca9bfbf8533ff8d507652c8b3a2
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-01-30 09:15:10 -0300

    Adicionando os actions modelos da ProjectsController e a classe DTO modelo

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers
 {
@@ -6,10 +7,61 @@ namespace DevFreela.API.Controllers
     [ApiController]
     public class ProjectsController : ControllerBase
     {
+        // api/projects?query=net core
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string query)
         {
+            // Buscar todos ou filtrar
+
             return Ok();
+        }
+
+        // api/projects/3
+        [HttpGet("{id:int}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            // Buscar o projeto
+
+            // return NotFound();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            // Cadastrar o projeto
+
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        // api/projects/2
+        [HttpPut("{id:int}")]
+        public IActionResult Put([FromRoute] int id,
+                                 [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            // Atualizo o projeto
+            return NoContent();
+        }
+
+        // api/projects/3
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            // Buscar, se não existir retorna NotFound
+
+            // Remover
+
+            return NoContent();
         }
     }
 }
