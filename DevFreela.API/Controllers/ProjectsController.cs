1ba4ccc40fb68735a0b0125be03d1398c10353e6
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-01-30 08:47:54 -0300

    Criando a class ProjectsController

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
