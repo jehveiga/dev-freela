c0e64c490dd6e4c0b240b264f35a317e9f4ab287
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-01-30 20:07:59 -0300

    Criando a class UsersController e a classe DTO CreateUserModel

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("{id:int}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserModel createUserModel)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 1 }, createUserModel);
+        }
+    }
+}
