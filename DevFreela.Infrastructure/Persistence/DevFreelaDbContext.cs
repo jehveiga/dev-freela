66fab2fb6058e6707a7ca5c803a8419232b070f6
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-02-05 21:10:35 -0300

    Adicionando os pacotes do EntityFramework e configurando a classe DevFreelaDbContext

--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,37 +1,14 @@
 ﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.Infrastructure.Persistence
 {
-    public class DevFreelaDbContext
+    public class DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : DbContext(options)
     {
-        public DevFreelaDbContext()
-        {
-            // Listas que será usada para testes
-            Projects =
-            [
-                new("Meu projeto ASPNET Core 1", "Minha Descrição de Projeto 1", 1, 1, 10000),
-                new("Meu projeto ASPNET Core 2", "Minha Descrição de Projeto 2", 1, 1, 20000),
-                new("Meu projeto ASPNET Core 3", "Minha Descrição de Projeto 3", 1, 1, 30000)
-            ];
-
-            Users =
-            [
-                new("Batman", "batman@empresawayne.com", new DateTime(1985, 1, 1)),
-                new("Coringa", "coringa@loucuras.com", new DateTime(1982, 3, 13)),
-                new("Robin", "robin@empresawayne.com", new DateTime(1997, 5, 25))
-            ];
-
-            Skills =
-                [
-                    new(".NET Core"),
-                    new("C#"),
-                    new("SQL"),
-                ];
-        }
-
-        public List<ProjectComment> ProjectComments { get; set; }
-        public List<Project> Projects { get; set; }
-        public List<User> Users { get; set; }
-        public List<Skill> Skills { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
     }
 }
