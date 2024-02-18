f3189fad4badabb086918cff464700c94e23bd70
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-02-18 13:41:27 -0300

    Implementando a criptografia na senha do usuário

--- DevFreela.Infrastructure/Auth/AuthService.cs
+++ DevFreela.Infrastructure/Auth/AuthService.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace DevFreela.Infrastructure.Auth
@@ -13,6 +14,27 @@ namespace DevFreela.Infrastructure.Auth
         public AuthService(IConfiguration configuration)
         {
             _configuration = configuration;
+        }
+
+        // Método que será usado para Criptografar a senha
+        public string ComputeSha256Hash(string password)
+        {
+
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - retorna byte array
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                // Converte byte array para string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2")); // x2 faz com que seja convertido em representação hexadecimal
+                }
+
+                return builder.ToString();
+            }
+
         }
 
         public string GenerateJwtToken(string email, string role)
