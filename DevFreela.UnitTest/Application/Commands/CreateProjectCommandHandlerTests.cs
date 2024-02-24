53a90c316925b30424fe8b15d71538f33660d33b
Jefferson Veiga <gelsvsampa@hotmail.com>
2024-02-23 21:18:10 -0300

    Criando o teste CreateProjectCommandHandlerTests

---
+++ DevFreela.UnitTest/Application/Commands/CreateProjectCommandHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Core.Entities;
+using DevFreela.Infrastructure.Persistence.Repositories;
+using NSubstitute;
+
+namespace DevFreela.UnitTest.Application.Commands
+{
+    public class CreateProjectCommandHandlerTests
+    {
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnProjectId()
+        {
+            // Arrange
+            var projectRepositoryMock = Substitute.For<IProjectRepository>();
+
+            var createProjectCommand = new CreateProjectCommand
+            {
+                Title = "Titulo de Teste",
+                Description = "Description de Teste",
+                IdClient = 1,
+                IdFreelancer = 2,
+                TotalCost = 5000
+            };
+
+            var createProjectCommandHandler = new CreateProjectCommandHandler(projectRepositoryMock);
+            var cancellationToken = new CancellationToken();
+            // Act
+            var id = await createProjectCommandHandler.Handle(createProjectCommand, cancellationToken);
+
+            // Assert
+            Assert.True(id >= 0);
+
+            await projectRepositoryMock.Received().AddAsync(Arg.Any<Project>());
+        }
+    }
+}
